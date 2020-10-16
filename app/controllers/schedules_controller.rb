1a2fb0a2efeec000a9d392abfebb754e77432685
kota-kaneta <foruri@icloud.com>
2020-10-16 17:39:57 +0900

    スケ変更機能

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,8 @@
 class SchedulesController < ApplicationController
+  before_action :set_schedule, only: [:edit, :update]
+  
   def index
-    @schedule = Schedule.all
+    @schedule = current_user.schedule
   end
 
   def new
@@ -21,15 +23,27 @@ class SchedulesController < ApplicationController
   end
 
   def edit
-    @schedule = Schedule.find(params[:id])
   end
 
   def update
+    if @schedule.update(schedule_edit_params)
+      redirect_to user_path(current_user.schedule)
+    else
+      render action: :edit
+    end
   end
 
   private
 
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
+
   def schedule_params
     params.require(:schedule).permit(:from_time_sun, :to_time_sun, :from_time_mon, :to_time_mon, :from_time_tue, :to_time_tue, :from_time_wed, :to_time_wed, :from_time_thu, :to_time_thu, :from_time_fri, :to_time_fri, :from_time_sat, :to_time_sat, :location).merge(user_id: current_user.id)
+  end
+
+  def schedule_edit_params
+    params.permit(:from_time_sun, :to_time_sun, :from_time_mon, :to_time_mon, :from_time_tue, :to_time_tue, :from_time_wed, :to_time_wed, :from_time_thu, :to_time_thu, :from_time_fri, :to_time_fri, :from_time_sat, :to_time_sat, :location).merge(user_id: current_user.id)
   end
 end
