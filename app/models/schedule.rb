8f3517acdfb662d19017c8c005863b623e25f09b
kota-kaneta <foruri@icloud.com>
2020-10-23 14:26:09 +0900

    取説

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,6 +3,6 @@ class Schedule < ApplicationRecord
 
   validates :location, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :from_time_sun, :to_time_sun, :from_time_mon, :to_time_mon, :from_time_tue, :to_time_tue, :from_time_wed, :to_time_wed, :from_time_thu, :to_time_thu, :from_time_fri, :to_time_fri, :from_time_sat, :to_time_sat,
-            numericality: { only_integer: true, greater_than: 0, less_than: 25 }, allow_blank: true
+            numericality: { only_integer: true, less_than: 25 }, allow_blank: true
 
 end
