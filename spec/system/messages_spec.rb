0d4cd1082b01f6db00262dc043c959fe406ce333
kota-kaneta <foruri@icloud.com>
2020-10-27 21:59:59 +0900

    連結テストコードmessage

---
+++ spec/system/messages_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe "Messages", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  context 'メッセージの送信に成功したとき' do
+
+    it 'ルーム内でDMの送信が成功したとき' do
+
+      # DMルームへ遷移
+      follow(@user)
+      click_on('DMルーム作成')
+
+      # メッセージを入力する
+      @message = FactoryBot.build(:message)
+      fill_in 'message[content]', with: @message
+
+      # メッセージを送信するとDBに保存される
+      expect{
+        find('input[name="commit"]').click
+      }.to change{ Message.count }.by(1)
+
+      # メッセージを送信するとルームに表示されている
+      expect(page).to have_content(@message)
+    end
+
+    it 'ルーム内でDMの送信が失敗したとき' do
+
+      # DMルームへ遷移
+      follow(@user)
+      click_on('DMルーム作成')
+
+      # メッセージが空のまま送信しようとする
+      @message = FactoryBot.build(:message)
+      @message = nil
+
+      # メッセージが保存されていないこと
+      expect{
+        find('input[name="commit"]').click
+      }.to change{ Message.count }.by(0)
+    end
+  end
+end
